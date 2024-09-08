78381ab34887fc42c2b93306ea90bdfbaa483379
Roman Cherepanov <romach007@gmail.com>
2024-09-08 23:35:48 +0300

    initial commit

---
+++ src/test/java/com/romach007/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romach007.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
